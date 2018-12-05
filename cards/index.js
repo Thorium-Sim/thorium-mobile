71e3fa07ac9646ce4bb460ac133465cc155c4316
Alex Anderson <alexanderson1993@gmail.com>
2018-12-05 10:09:34 -0700

    Add Engineering reports and RnD Reports to the mobile app.

--- cards/index.js
+++ cards/index.js
@@ -3,3 +3,5 @@ export { default as Keypad } from "./Keypad";
 export { default as MedicalTricorder } from "./MedicalTricorder";
 export { default as Tricorder } from "./Scanner";
 export { default as DamageReports } from "./DamageReports";
+export { default as EngineeringReports } from "./DamageReports/engineering";
+export { default as RNDReports } from "./DamageReports/rnd";
